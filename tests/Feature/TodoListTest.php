86bf7ab899bae83e5d4e6b24d8b7330cacbb6b48
jesus-son-coder <rvck2007@gmail.com>
2022-06-07 23:34:50 +0200

    initialisation du projet

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        // preparation
+
+        // action
+
+        // assertion
+    }
+}
