7559c47c08e687d81b4b5a858417f19bdb8373eb
jesus-son-coder <rvck2007@gmail.com>
2022-06-08 15:18:24 +0200

    Unit-test de la nouvelle route "Store" pour créer une nouvelle Ressource (TodoList)

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -4,20 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\TodoList;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 
 class TodoListController extends Controller
 {
-    public function index()
+    public function index(): Response|Application|ResponseFactory
     {
         $lists = TodoList::all();
-        // return response()->json(['lists' => []]);
-        // ou
+
         return response($lists);
     }
 
-    public function show(TodoList $todolist)
+    public function show(TodoList $todolist): Response|Application|ResponseFactory
     {
         return response($todolist);
     }
+
+    public function store(Request $request)
+    {
+        TodoList::create(['name' => $request->name]);
+
+        return response('', 201);
+    }
+
 }
 
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -24,7 +24,7 @@ class TodoListTest extends TestCase
     public function test_fetch_all_todo_list(): void
     {
         // Action / Perform :
-        $response = $this->getJson(route('todo-list.store'));
+        $response = $this->getJson(route('todo-list.index'));
 
         // Assertion / Predict
         $this->assertCount(1, $response->json());
@@ -41,7 +41,7 @@ class TodoListTest extends TestCase
         $this->assertEquals($response->json()['name'], $this->list->name);
     }
 
-    public function test_fetch_single_todo_list_optimise()
+    public function test_fetch_single_todo_list_optimized()
     {
         // Action / Perform :
         $response = $this->getJson(route('todo-list.show', $this->list->id))
@@ -50,5 +50,26 @@ class TodoListTest extends TestCase
 
         // Assertion / Predict :
         $this->assertEquals($response['name'], $this->list->name);
+    }
+
+    public function test_store_new_todo_list()
+    {
+        // Preperation :
+
+
+        // Action / Perform :
+        $this->postJson(route('todo-list.store'), [
+            'name' => 'my NEW list'
+        ])
+            ->assertStatus(201);
+            // ou "assertSuccessful()" => c'est pareil que le "201" : pour ressource créée !
+            // ou encore "assertCreated()" => pour la ressource créée avec succès !
+
+        // Assertion / Predict :
+        /*
+         * We expect to have the data "['name' => 'my NEW list']"
+         * in the Table called "todo_lists" :
+         */
+        $this->assertDatabaseHas('todo_lists', ['name' => 'my NEW list']);
     }
 }
